934fb50ec934c287911f150d2cddad27bcff1500
jason murphy <jmurphy.1196@gmail.com>
2023-07-28 14:12:09 -0700

    added styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import { ModalProvider, Modal } from "./context/Modal";
 import configureStore from "./store";
 import * as sessionActions from "./store/session";
 import App from "./App";
+import "./styles/globals/main.css";
 
 const store = configureStore();
 
