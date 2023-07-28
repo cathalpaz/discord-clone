8cab456ab90fd62bdb59c830286f1d9177110c9d
jason murphy <jmurphy.1196@gmail.com>
2023-07-28 16:33:09 -0700

    added util folder and moved a function over to it

--- backend/app/api/auth_routes.py
+++ backend/app/api/auth_routes.py
@@ -4,19 +4,9 @@ from ..forms import LoginForm
 from ..forms import UserForm
 from flask_login import current_user, login_user, logout_user, login_required
 from sqlalchemy import or_
+from ..utils.validate_errors import validation_errors_to_error_messages
 
 auth_routes = Blueprint('auth', __name__)
-
-
-def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f'{field} : {error}')
-    return errorMessages
 
 
 @auth_routes.route('/')
