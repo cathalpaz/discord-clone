c57cb4560c35f0de7ead3082e2d64a0df838378e
jason murphy <jmurphy.1196@gmail.com>
2023-07-28 16:05:25 -0700

    updated signup route

--- backend/app/forms/user_form.py
+++ backend/app/forms/user_form.py
@@ -2,22 +2,25 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, DateField, FileField, SelectMultipleField
 from wtforms.validators import DataRequired, ValidationError, URL, Length
 
+
 def image_validator(form, field):
-    if not form.data["avatar"].endswith("jpg", "png", "jpeg"):
-        raise ValidationError("Needs to end with jpg, png, jpeg")
-    elif URL(form.data, message="Not a valid URL"):
-        raise ValidationError("Not a valid URL")
+    if not (form.data['avatar'].endswith('.jpg') or form.data["avatar"].endswith(".png") or form.data["avatar"].endswith('.jpeg')):
+        raise ValidationError("Invalid photo")
+
 
 def email_validator(form, field):
-    if "@" not in  field.data:
+    if "@" not in field.data:
         raise ValidationError("Must be a valid email")
 
+
 class UserForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min=4, max=50)])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=4, max=50)])
+    username = StringField("Username", validators=[
+                           DataRequired(), Length(min=4, max=50)])
+    password = PasswordField("Password", validators=[
+                             DataRequired(), Length(min=4, max=50)])
     email = EmailField("Email", validators=[DataRequired()])
-    birthday = DateField("Birthday", validators=[DataRequired()])
+    birthday = StringField("Birthday", validators=[DataRequired()])
     banner_color = StringField("Banner Color", validators=[DataRequired()])
     bio = StringField("Bio", validators=[DataRequired()])
-    pronouns = SelectMultipleField("Pronouns", validators=[DataRequired()])
-    avatar = FileField("Avatar", validators=[DataRequired(),image_validator])
+    pronouns = StringField("Pronouns", validators=[DataRequired()])
+    avatar = FileField("Avatar", validators=[DataRequired(), image_validator])
