ccacf5eaa73654e10d2692b52539f3a6e0c45a1b
jason murphy <jmurphy.1196@gmail.com>
2023-07-29 15:38:55 -0700

    created channel message route

--- backend/app/forms/__init__.py
+++ backend/app/forms/__init__.py
@@ -3,5 +3,5 @@ from .signup_form import SignUpForm
 from .user_form import UserForm
 from .server_profile import ServerProfile
 from .user_message import UserMessage
-from .channel_message import ChannelMessage
+from .channel_message import ChannelMessageForm
 from .reaction import Reaction
