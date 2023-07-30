cb2bb9527728b33971ae51fddbd9ca404244bd6a
Cathal <cathalpaz@gmail.com>
2023-07-30 14:15:48 -0400

    get friends route

--- backend/app/api/direct_messages_routes.py
+++ backend/app/api/direct_messages_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_login import current_user, login_required
-from ..models import DirectMessage, User
-from sqlalchemy import or_
+from ..models import DirectMessage, User, Friend
+from sqlalchemy import or_, and_
 
 direct_messages_routes = Blueprint('direct_messages', __name__, url_prefix='/@me')
 
@@ -16,3 +16,18 @@ def all_direct_messages():
     )).all()
     dms = [dm.to_dict() for dm in get_dms]
     return {'dms': dms}
+
+
+# GET all friends
+@direct_messages_routes.route('/friends')
+@login_required
+def all_friends():
+    get_friends = Friend.query.filter(or_(
+        Friend.user_to == current_user.id,
+        Friend.user_from == current_user.id
+    )).all()
+    friends = [friend.to_dict() for friend in get_friends if friend.status == 'FRIENDS']
+    return {'friends': friends}
+
+
+# CREATE new direct message
