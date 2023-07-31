4164a5f6a11717b8dce8fd18831ea8b64a1f65c9
Cathal <cathalpaz@gmail.com>
2023-07-31 19:27:51 -0400

    user profile folder

---
+++ frontend/src/components/UserProfile/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import OpenModalButton from '../OpenModalButton'
+
+
+function UserProfile() {
+  return (
+    <div>UserProfile</div>
+  )
+}
+
+export default UserProfile
