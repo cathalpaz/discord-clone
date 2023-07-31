e7bc6a4471d1e2cd810b1dede968bc36c385bfaa
jason murphy <jmurphy.1196@gmail.com>
2023-07-31 16:26:48 -0700

    fixed server list

--- frontend/src/store/server.js
+++ frontend/src/store/server.js
@@ -11,12 +11,11 @@ const getServers = (servers) => {
 };
 
 const createServer = (server) => {
-    return {
-        type: CREATE_SERVER,
-        server
-    }
-}
-
+  return {
+    type: CREATE_SERVER,
+    server,
+  };
+};
 
 // thunk action creator
 export const thunkGetAllServers = () => async (dispatch) => {
@@ -30,47 +29,49 @@ export const thunkGetAllServers = () => async (dispatch) => {
 };
 
 export const thunkCreateServer = (server) => async (dispatch) => {
-    const res = await fetch('/api/servers/new', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(server)
-    })
-    if (res.ok) {
-        const data = await res.json()
-        dispatch(createServer(data))
-        return data
-    } else {
-        const errorData = await res.json()
-        return errorData
-    }
-}
-
-const initialState = {
-    orderedServers: []
+  const res = await fetch("/api/servers/new", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(server),
+  });
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(createServer(data));
+    return data;
+  } else {
+    const errorData = await res.json();
+    return errorData;
+  }
 };
 
-export default function serverReducer (state = initialState, action) {
-	switch (action.type) {
-		case GET_SERVERS: {
-            const newState = {...state}
-            const servers = action.servers.servers
-            const orderedServers = [...newState.orderedServers]
-            for (let server of servers) {
-                newState[server.id] = server
-                orderedServers.push(server.id)
-            }
-            newState.orderedServers = orderedServers
-            return newState
+const initialState = {
+  orderedServers: [],
+};
+
+export default function serverReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_SERVERS: {
+      const newState = { ...state };
+      const servers = action.servers.servers;
+      const orderedServers = [...state.orderedServers];
+      for (let server of servers) {
+        if (!newState[server.id]) {
+          orderedServers.push(server.id);
         }
-        case CREATE_SERVER: {
-            const newState = {...state}
-            newState[action.server.id] = action.server
-            let orderedServers = [...newState.orderedServers]
-            orderedServers.push(action.server.id)
-            newState.orderedServers = orderedServers
-            return newState
-        }
-		default:
-			return state;
-	}
+        newState[server.id] = server;
+      }
+      newState.orderedServers = orderedServers;
+      return newState;
+    }
+    case CREATE_SERVER: {
+      const newState = { ...state };
+      newState[action.server.id] = action.server;
+      const orderedServers = [...newState.orderedServers];
+      orderedServers.push(action.server.id);
+      newState.orderedServers = orderedServers;
+      return newState;
+    }
+    default:
+      return state;
+  }
 }
