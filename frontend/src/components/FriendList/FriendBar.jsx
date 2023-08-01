c1074bf7a0a9a4c4b1b14fd4a20154c0ad7b1a20
Zachary <stallings.zach@icloud.com>
2023-08-01 11:02:57 -0700

    fixed friends button

--- frontend/src/components/FriendList/FriendBar.jsx
+++ frontend/src/components/FriendList/FriendBar.jsx
@@ -1,54 +1,54 @@
-import React, { useEffect }  from 'react'
+import React, { useEffect }  from 'react';
 
 export default function FriendBar({ selectedTab, setSelectedTab }) {
 
     useEffect(() => {
-        setSelectedTab("Online")
-    },[setSelectedTab])
+        setSelectedTab("Online");
+    }, [setSelectedTab]);
 
-  return (
-    <div className="friends-bar">
-    <div className="general-view-header">
-      <div className="friends-h1">
-        <i className="fa-solid fa-users"></i>
-        <h2 style={{fontSize:"16px", fontWeight:"600"}}>Friends</h2>
-      </div>
-      <div className="divider"></div>
-      <nav className="app-nav">
-        <div className="text-bar">
-            <button
-              className={
-                selectedTab === "Online"
-                  ? "active nav-button online-friend"
-                  : "nav-button online-friend"
-              }
-              onClick={() => setSelectedTab("Online")}
-            >
-              Online
-            </button>
-            <button
-              className={
-                selectedTab === "All"
-                  ? "active nav-button online-friend"
-                  : "nav-button online-friend"
-              }
-              onClick={() => setSelectedTab("All")}
-            >
-              All
-            </button>
-            <button
-              className={
-                selectedTab === "Add"
-                  ? "active nav-button add-friend"
-                  : "nav-button add-friend"
-              }
-              onClick={() => setSelectedTab("Add")}
-            >
-              Add Friend
-            </button>
+    return (
+        <div className="friends-bar">
+            <div className="general-view-header">
+                <div className="friends-h1">
+                    <i className="fa-solid fa-users"></i>
+                    <h2 style={{ fontSize: "16px", fontWeight: "600" }}>Friends</h2>
+                </div>
+                <div className="divider"></div>
+                <nav className="app-nav">
+                    <div className="text-bar">
+                        <button
+                            className={
+                                selectedTab === "Online"
+                                    ? "active nav-button online-friend"
+                                    : "nav-button online-friend"
+                            }
+                            onClick={() => setSelectedTab("Online")}
+                        >
+                            Online
+                        </button>
+                        <button
+                            className={
+                                selectedTab === "All"
+                                    ? "active nav-button online-friend"
+                                    : "nav-button online-friend"
+                            }
+                            onClick={() => setSelectedTab("All")}
+                        >
+                            All
+                        </button>
+                        <button
+                            className={
+                                selectedTab === "Add"
+                                    ? "active nav-button add-friend"
+                                    : "nav-button add-friend"
+                            }
+                            onClick={() => setSelectedTab("Add")}
+                        >
+                            Add Friend
+                        </button>
+                    </div>
+                </nav>
+            </div>
         </div>
-      </nav>
-    </div>
-      </div>
-  )
+    );
 }
--- frontend/src/components/FriendList/index.jsx
+++ frontend/src/components/FriendList/index.jsx
@@ -1,85 +1,80 @@
-import { NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "../../styles/components/FriendList.css";
 import { fetchFriends } from "../../store/session";
 
-function FriendList({ isLoaded, selectedTab}) {
-  const sessionUser = useSelector((state) => state.session.user);
-  const friendStore = useSelector((state) => state.session.friends)
-  const [selectedNavItem, setSelectedNavItem] = useState("online");
-  const [searchQuery, setSearchQuery] = useState("");
-  const [onlineFriends, setOnlineFriends] = useState([]);
-  const [allFriends, setAllFriends] = useState([]);
-  const dispatch = useDispatch();
+function FriendList({ selectedTab }) {
+    const sessionUser = useSelector((state) => state.session.user);
+    const friendStore = useSelector((state) => state.session.friends);
+    const [selectedNavItem, setSelectedNavItem] = useState("online");
+    const [searchQuery, setSearchQuery] = useState("");
+    const [onlineFriends, setOnlineFriends] = useState([]);
+    const [allFriends, setAllFriends] = useState([]);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchFriends())
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchFriends());
+    }, [dispatch]);
 
-  console.log('friendstore', friendStore)
+    const searchMessages = (event) => {
+        setSearchQuery(event.target.value);
+    };
 
-
-  const searchMessages = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const renderContent = () => {
-    if (selectedNavItem === "online") {
-        return (
-        <div className="content-online">
-            <h2>{selectedTab}</h2>
-            {friendStore &&
-            friendStore.map((friend) => (
-                <div key={friend.id}>{friend.user_from}
+    const renderContent = () => {
+        if (selectedTab === "Online") {
+            return (
+                <div className="content-online">
+                    <h2>{selectedTab}</h2>
+                    {friendStore &&
+                        friendStore.map((friend) => (
+                            <div key={friend.id}>{friend.user_from}</div>
+                        ))}
                 </div>
-            ))}
-        </div>
-        );
-    } else if (selectedNavItem === "all") {
-        return (
-        <div className="content-all">
-            <h2>{selectedTab}</h2>
-            {friendStore &&
-            friendStore.map((friend) => (
-                <div key={friend.id}>
-                <p>{friend.id}</p>
-                <p>{friend.user_from}</p>
+            );
+        } else if (selectedTab === "All") {
+            return (
+                <div className="content-all">
+                    <h2>{selectedTab}</h2>
+                    {friendStore &&
+                        friendStore.map((friend) => (
+                            <div key={friend.id}>
+                                <p>{friend.id}</p>
+                                <p>{friend.user_from}</p>
+                            </div>
+                        ))}
                 </div>
-            ))}
-        </div>
-        );
-    } else if (selectedNavItem === "add") {
-        return (
-        <div className="content-add">
-            <div>Add Friend</div>
-            <div>You can add friends with their Discord username</div>
-        </div>
-        );
-    }
-    return null;
-  };
+            );
+        } else if (selectedTab === "Add") {
+            return (
+                <div className="content-add">
+                    <div>Add Friend</div>
+                    <div>You can add friends with their Discord username</div>
+                </div>
+            );
+        }
+        return null;
+    };
 
-  let asd = document.querySelector(".active")
-  console.log('asd', asd)
-  return (
+    let asd = document.querySelector(".active");
+    console.log('asd', asd);
 
-    <div className="discord">
-      <div className="search-container">
-        <div className="search-input-container">
-          <input
-            type="text"
-            placeholder="Search"
-            value={searchQuery}
-            onChange={searchMessages}
-            className="search-input-bar"
-          />
-          <i className="fa-solid fa-magnifying-glass search-icon"></i>
+    return (
+        <div className="discord">
+            <div className="search-container">
+                <div className="search-input-container">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        value={searchQuery}
+                        onChange={searchMessages}
+                        className="search-input-bar"
+                    />
+                    <i className="fa-solid fa-magnifying-glass search-icon"></i>
+                </div>
+            </div>
+            <div className="content-container">{renderContent()}</div>
         </div>
-      </div>
-      <div className="content-container">{renderContent()}</div>
-    </div>
-  );
+    );
 }
 
 export default FriendList;
