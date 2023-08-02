b4c5adcd6cf341ecb128a91361433c359a645027
Zachary <stallings.zach@icloud.com>
2023-08-02 08:50:58 -0700

    fixed search and hover

--- frontend/src/components/FriendList/index.jsx
+++ frontend/src/components/FriendList/index.jsx
@@ -6,21 +6,9 @@ import { fetchFriends } from "../../store/session";
 function FriendList({ selectedTab }) {
   const sessionUser = useSelector((state) => state.session.user);
   const friendStore = useSelector((state) => state.session.friends);
-  const [selectedNavItem, setSelectedNavItem] = useState("online");
   const [searchQuery, setSearchQuery] = useState("");
-  const [onlineFriends, setOnlineFriends] = useState([]);
-  const [allFriends, setAllFriends] = useState([]);
-  const [isAddingFriend, setIsAddingFriend] = useState(false);
   const dispatch = useDispatch();
   const [hoverStates, setHoverStates] = useState({});
-
-  //hovering a name shows the user ID
-  const handleFriendHover = (friendId, isHovering) => {
-    setHoverStates((prevState) => ({
-      ...prevState,
-      [friendId]: isHovering,
-    }));
-  };
 
   useEffect(() => {
     dispatch(fetchFriends());
@@ -30,62 +18,51 @@ function FriendList({ selectedTab }) {
     setSearchQuery(event.target.value);
   };
 
-  //this is so we can style the "Online" & "All" text.
-  const headingClassName = "heading-class";
-
-  //make friends list go away if not on correct tab
-  const handleAddFriendClick = () => {
-    setIsAddingFriend(!isAddingFriend);
+  const handleFriendHover = (friendId, isHovering) => {
+    setHoverStates((prevState) => ({
+      ...prevState,
+      [friendId]: isHovering,
+    }));
   };
-  console.log(searchQuery);
 
-  let filteredFriends = [];
-
-  if (friendStore?.length) {
-    filteredFriends = friendStore.filter((friend) =>
-      friend.user.username.includes(searchQuery)
-    );
-  }
-
-  console.log(filteredFriends);
+  const filteredFriends = friendStore.filter((friend) =>
+    friend.user.username.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   const renderContent = () => {
     if (selectedTab === "Online") {
       return (
         <div className="content-online">
-          <h2 className={headingClassName}>{selectedTab}</h2>
-          {friendStore &&
-            friendStore.map((friend) => (
-              <div
-                key={friend.id}
-                onMouseEnter={() => handleFriendHover(friend.id, true)}
-                onMouseLeave={() => handleFriendHover(friend.id, false)}
-              >
-                <img src={friend.user.avatar} alt="" />
-                <p>{friend.user.username}</p>
-                {hoverStates[friend.id] && <p>#{friend.id}</p>}
-              </div>
-            ))}
+          <h2 className="heading-class">{selectedTab}</h2>
+          {filteredFriends.map((friend) => (
+            <div
+              key={friend.id}
+              onMouseEnter={() => handleFriendHover(friend.id, true)}
+              onMouseLeave={() => handleFriendHover(friend.id, false)}
+            >
+              <img src={friend.user.avatar} alt="" />
+              <p>{friend.user.username}</p>
+              {hoverStates[friend.id] && <p>#{friend.id}</p>}
+            </div>
+          ))}
         </div>
       );
     } else if (selectedTab === "All") {
       return (
         <div className="content-all">
-          <h2 className={headingClassName}>{selectedTab}</h2>
-          {friendStore &&
-            friendStore.map((friend) => (
-              <div
-                key={friend.id}
-                onMouseEnter={() => handleFriendHover(friend.id, true)}
-                onMouseLeave={() => handleFriendHover(friend.id, false)}
-                className="friend-container" // Add the friend-container class here
-              >
-                <img src={friend.user.avatar} alt="" />
-                <p>{friend.user.username}</p>
-                {hoverStates[friend.id] && <p>#{friend.id}</p>}
-                <i className="fa-solid fa-message"></i>
-              </div>
-            ))}
+          <h2 className="heading-class">{selectedTab}</h2>
+          {filteredFriends.map((friend) => (
+            <div
+              key={friend.id}
+              onMouseEnter={() => handleFriendHover(friend.id, true)}
+              onMouseLeave={() => handleFriendHover(friend.id, false)}
+            >
+              <img src={friend.user.avatar} alt="" />
+              <p>{friend.user.username}</p>
+              {hoverStates[friend.id] && <p>#{friend.id}</p>}
+              <i className="fa-solid fa-message"></i>
+            </div>
+          ))}
         </div>
       );
     } else if (selectedTab === "Add") {
