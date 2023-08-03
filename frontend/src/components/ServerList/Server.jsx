60bdf7a83ddf575b7dc061dea20d132f8a004d02
thejhp1 <junhyung96@gmail.com>
2023-08-03 19:26:39 -0400

    serverlist refactor

--- frontend/src/components/ServerList/Server.jsx
+++ frontend/src/components/ServerList/Server.jsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export function Server({ serverId }) {
   const server = useSelector((state) => state.servers[serverId]);
+  const userStore = useSelector((state) => state.session.user)
   if (!server) return false;
   const history = useHistory();
   const handleClick = (e) => {
@@ -10,15 +11,20 @@ export function Server({ serverId }) {
     history.push(`/${server.id}/${server.channels[0].id}`);
   };
   return (
-    <div className='server__container' onClick={handleClick}>
-      <div className='server__img-container'>
-        <img
-          className='serverlist-icon .tooltip-container'
-          src={server.avatar}
-          alt=''
-        />
-        <div className='server__img-tooltip'>{server.name}</div>
+    <>
+      {server.owner.id == userStore.id || server.users.map(user => user.id == userStore.id ) ?
+      <div className='server__container' onClick={handleClick}>
+        <div className='server__img-container'>
+          <img
+            style={{ width: "3rem", height: "3rem" }}
+            className='serverlist-icon .tooltip-container'
+            src={server.avatar}
+            alt=''
+          />
+          <div className='server__img-tooltip'>{server.name}</div>
+        </div>
       </div>
-    </div>
+        : "" }
+    </>
   );
 }
