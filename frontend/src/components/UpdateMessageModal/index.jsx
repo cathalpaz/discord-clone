e4227bfe0eae83665ca6c81b92787cbbcbb72c3b
Cathal <cathalpaz@gmail.com>
2023-08-03 19:10:09 -0400

    edit messasmodal

--- frontend/src/components/UpdateMessageModal/index.jsx
+++ frontend/src/components/UpdateMessageModal/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from 'react'
+import { useModal } from '../../context/Modal'
 import '../../styles/components/UpdateMessageModal.css'
 
 function UpdateMessageModal({ message }) {
+  const { closeModal } = useModal()
+  let [newMessage, setNewMessage] = useState(message.content)
+
+  const handleEnter = (e) => {
+    if (e.key === 'Enter') {
+      updateMessage()
+    }
+  }
+
+  const updateMessage = (e) => {
+    e.preventDefault()
+    // handle socket stuff here
+    console.log('SUBMITTED')
+    closeModal()
+  }
+
   return (
-    <div className='update-message__container'>UpdateMessageModal</div>
+    <div className='update-message__container'>
+      <div>Update message</div>
+      <label>
+        Message:
+        <textarea
+          onKeyDown={handleEnter}
+          value={newMessage}
+          onChange={(e) => e.target.value}
+          >
+        </textarea>
+      </label>
+      <div className='update-message__btns'>
+        <button onClick={closeModal}>Cancel</button>
+        <button>Save</button>
+      </div>
+    </div>
   )
 }
 
