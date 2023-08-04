528e1ddb11e8708a8741bdf5969713c23c14ad39
thejhp1 <junhyung96@gmail.com>
2023-08-03 22:45:07 -0400

    friends list

--- frontend/src/components/FriendList/index.jsx
+++ frontend/src/components/FriendList/index.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "../../styles/components/FriendList.css";
 import { fetchFriends, thunkSendFriendRequest } from "../../store/session";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function FriendList({ selectedTab }) {
-  const sessionUser = useSelector((state) => state.session.user);
   const friendStore = useSelector((state) => state.session.friends);
   const [searchQuery, setSearchQuery] = useState("");
-  const [addFriend, setAddFriend] = useState("");
-  const dispatch = useDispatch();
   const [hoverStates, setHoverStates] = useState({});
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(fetchFriends());
@@ -27,42 +27,48 @@ function FriendList({ selectedTab }) {
   };
 
   const sendFriendRequest = () => {
-    dispatch(thunkSendFriendRequest(searchQuery))
-  }
-
-  //add friend button to direct you to Add tab
-  const handleAddFriendClick = () => {
-    setSelectedTab("Add");
+    dispatch(thunkSendFriendRequest(searchQuery));
   };
 
+  const sendToDiscovery = () => {
+    history.push("/discovery")
+  }
 
   let filteredFriends = [];
   let pendingFriends = [];
   if (friendStore) {
     filteredFriends = friendStore.filter(
       (friend) =>
-        friend.user.username.toLowerCase().includes(searchQuery.toLowerCase()) &&
-        friend.status === "ACCEPTED"
+        friend.user.username
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()) && friend.status === "ACCEPTED"
     );
     pendingFriends = friendStore.filter(
       (friend) =>
-        friend.user.username.toLowerCase().includes(searchQuery.toLowerCase()) &&
-        friend.status === "PENDING"
+        friend.user.username
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()) && friend.status === "PENDING"
     );
   }
 
   const renderContent = () => {
+    // setSearchQuery("")
     if (selectedTab === "Online") {
+      // setSearchQuery(n)
       return (
         <div className="content-online">
-          <h2 className="heading-class">{selectedTab}</h2>
+          <h2 className="heading-class">{filteredFriends.length === 0 ? "" : selectedTab} {filteredFriends.length !== 0 ? filteredFriends.length : ""}</h2>
           {filteredFriends.length === 0 ? (
-            // <div className="content-add">
-            <div>
-              <p className="offline-wumpus-text">No one's around to play with Wumpus.</p>
-              <img className="offline-wumpus"src="../../../public/images/NoOnline.svg" alt="" />
+            <div className="no-friend-container">
+              <img
+                className="offline-wumpus"
+                src={"/images/NoOnline.svg"}
+                alt=""
+              />
+              <p className="no-friends-txt">
+                No one's around to play with Wumpus.
+              </p>
             </div>
-            // </div>
           ) : (
             filteredFriends.map((friend) => (
               <div
@@ -88,18 +94,21 @@ function FriendList({ selectedTab }) {
       );
     } else if (selectedTab === "All") {
       return (
-        <div className="empty-block">
-          <h2 className="heading-class">{selectedTab}</h2>
+        <div className="content-online">
+          <h2 className="heading-class">{filteredFriends.length === 0 ? "" : selectedTab} {filteredFriends.length !== 0 ? filteredFriends.length : ""}</h2>
           {filteredFriends.length === 0 ? (
-                        <div>
-                        <p className="no-friends-txt">Wumpus is waiting on friends. You don't have to though!</p>
-                        <img className="no-friends" src="../../../public/images/NoFriends.svg" alt="" />
-                        <div>
-                          <button className="send-request-all" onClick={handleAddFriendClick}>
-                            Add Friend
-                          </button>
-                        </div>
-                      </div>
+            <div className="no-friend-container">
+              <img
+                className="no-friends"
+                src={"/images/NoFriends.svg"}
+                alt=""
+              />
+              <p className="no-friends-txt">
+                Wumpus is waiting on friends. You don't have to though!
+              </p>
+              <div>
+              </div>
+            </div>
           ) : (
             filteredFriends.map((friend) => (
               <div
@@ -131,7 +140,7 @@ function FriendList({ selectedTab }) {
             You can add friends with their Discord username
           </div>
           <div className="Add-Input">
-          <input
+            <input
               type="text"
               placeholder="You can add friends with their Discord username."
               value={searchQuery}
@@ -139,34 +148,45 @@ function FriendList({ selectedTab }) {
               className="add-a-friend-bar"
             />
             <div className="Add-button-container">
-              <button
-              onClick={sendFriendRequest}
-              className="send-request"
-              >Send Friend Request
+              <button onClick={sendFriendRequest} className="send-request">
+                Send Friend Request
               </button>
-
             </div>
-
           </div>
+          <div className="friend-list-border"></div>
+          <div className="friend-list-discovery-container">
+            <div className="Add-Friend">
+              OTHER PLACES TO MAKES FRIENDS
+            </div>
+            <span onClick={sendToDiscovery} className="friend-list-discovery-button">
+              <i class="fa-solid fa-compass friend-list-discovery-icon"></i>
+              <p style={{fontWeight:"500", color:"var(--light-accent)", marginLeft:".5rem"}}>Explore Discoverable Servers</p>
+              <i style={{color:"var(--light-accent)", marginLeft:"1.2rem", fontSize:"18px"}}class="fa-solid fa-chevron-right"></i>
+            </span>
+          </div>
+          <div className="no-friend-container">
             <div className="loner-wumpus">
-            <img src="../../../public/images/wumpus-add-friend.svg" alt="" />
+              <img src="../../../public/images/wumpus-add-friend.svg" alt="" />
             </div>
             <div>
               <p className="loner-wumpus-text">
                 Wumpus is waiting on friends. You don't have to though!
-                </p>
+              </p>
             </div>
+          </div>
         </div>
       );
     } else if (selectedTab === "Pending") {
       return (
         <div className="content-all">
-          <h2 className="heading-class">{selectedTab}</h2>
+          <h2 className="heading-class">{pendingFriends.length === 0 ? "" : selectedTab} {pendingFriends.length !== 0 ? pendingFriends.length : ""}</h2>
           {pendingFriends.length === 0 ? (
-             <div>
-             <p>There are no pending friend requests. Here's Wumpus for now.</p>
-             <img src="../../../public/images/NoPending.svg" alt="" />
-           </div>
+            <div className="no-friend-container">
+              <img src="../../../public/images/NoPending.svg" alt="" />
+              <p className="no-friends-txt">
+                There are no pending friend requests. Here's Wumpus for now.
+              </p>
+            </div>
           ) : (
             pendingFriends.map((friend) => (
               <div
