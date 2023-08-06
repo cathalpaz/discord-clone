e34c651725b503c8eba584d573dd212036dad7b5
jason murphy <jmurphy.1196@gmail.com>
2023-08-06 13:50:50 -0700

    fixed

--- frontend/src/components/DirectMessage/DirectMessageUserSection.jsx
+++ frontend/src/components/DirectMessage/DirectMessageUserSection.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react'
-import "../../styles/components/DirectMessageUserSection.css"
-import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import React, { useState } from "react";
+import "../../styles/components/DirectMessageUserSection.css";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import dayjs from "dayjs";
 
 export default function DirectMessageUserSection() {
-    const directMessageStore = useSelector(state => state.directMessages.users)
-    const {directMessageId} = useParams()
-    const [note, setNote] = useState("")
-    console.log('store', directMessageStore[directMessageId])
-    const date = directMessageStore[directMessageId]?.created_at.split(" ")
-    const newDate = `${date[2]} ${date[1]} ${date[3]}`
+  const dmUsers = useSelector((state) => state.directMessages.users);
+  const { directMessageId } = useParams();
+  const [note, setNote] = useState("");
+  if (!dmUsers[directMessageId]) return false;
+  const newDate = dayjs(dmUsers[directMessageId].created_at).format(
+    "MM/DD/YYYY"
+  );
 
-    console.log('note', note)
+  console.log("note", note);
   return (
     <div className='direct-message-user-container'>
-        <div style={{backgroundColor:directMessageStore[directMessageId]?.banner, height:"10rem"}} className='direct-message-user-banner-container'>
-            <img src={directMessageStore[directMessageId]?.avatar}></img>
+      <div
+        style={{
+          backgroundColor: dmUsers[directMessageId]?.banner,
+          height: "10rem",
+        }}
+        className='direct-message-user-banner-container'
+      >
+        <img src={dmUsers[directMessageId]?.avatar}></img>
+      </div>
+      <div className='direct-message-user-info-container'>
+        <div className='direct-message-user-info'>
+          <p>{dmUsers[directMessageId]?.username}</p>
+          <p className='direct-message-user-info-divider'></p>
+          <p>SLACORD MEMBER SINCE</p>
+          <p>{newDate}</p>
+          <p className='direct-message-user-info-divider'></p>
+          <p>NOTE</p>
+          <input
+            placeholder='Click to add a note'
+            value={note}
+            onClick={() => alert("Feature coming soon!")}
+          ></input>
         </div>
-        <div className='direct-message-user-info-container'>
-            <div className='direct-message-user-info'>
-                <p>{directMessageStore[directMessageId]?.username}</p>
-                <p className='direct-message-user-info-divider'></p>
-                <p>SLACORD MEMBER SINCE</p>
-                <p>{newDate}</p>
-                <p className='direct-message-user-info-divider'></p>
-                <p>NOTE</p>
-                <input placeholder='Click to add a note' value={note} onClick={() =>alert('Feature coming soon!')}></input>
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
