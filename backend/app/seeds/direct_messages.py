b9dbb8d23c043a91741918a7f1328204c5df4b1b
Cathal <cathalpaz@gmail.com>
2023-08-07 00:06:45 -0400

    dm seeds

--- backend/app/seeds/direct_messages.py
+++ backend/app/seeds/direct_messages.py
@@ -8,25 +8,40 @@ def seed_direct_messages():
         user_to_id = 2,
         content = 'Hello, how are you?'
     )
-    direct_message_5 = DirectMessage(
+    direct_message_2 = DirectMessage(
         user_from_id = 1,
         user_to_id = 3,
         content = 'What are you doing later?'
     )
-    direct_message_2 = DirectMessage(
+    direct_message_3 = DirectMessage(
         user_from_id = 2,
         user_to_id = 1,
         content = 'Gr8!'
     )
-    direct_message_3 = DirectMessage(
-        user_from_id = 2,
-        user_to_id = 3,
-        content = 'You got games on your phone?'
-    )
     direct_message_4 = DirectMessage(
         user_from_id = 3,
-        user_to_id = 2,
-        content = 'Nah bro.'
+        user_to_id = 1,
+        content = 'Nada'
+    )
+    direct_message_5 = DirectMessage(
+        user_from_id = 1,
+        user_to_id = 3,
+        content = 'K.'
+    )
+    direct_message_6 = DirectMessage(
+        user_from_id = 1,
+        user_to_id = 5,
+        content = 'Hello friend'
+    )
+    direct_message_7 = DirectMessage(
+        user_from_id = 5,
+        user_to_id = 1,
+        content = 'wassup'
+    )
+    direct_message_8 = DirectMessage(
+        user_from_id = 2,
+        user_to_id = 4,
+        content = 'Answer me pls'
     )
 
     db.session.add(direct_message_1)
@@ -34,6 +49,9 @@ def seed_direct_messages():
     db.session.add(direct_message_3)
     db.session.add(direct_message_4)
     db.session.add(direct_message_5)
+    db.session.add(direct_message_6)
+    db.session.add(direct_message_7)
+    db.session.add(direct_message_8)
 
     db.session.commit()
 
