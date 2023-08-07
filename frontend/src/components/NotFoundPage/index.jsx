bc4471317ca134fcec7e288819eecf61b978c3bc
thejhp1 <junhyung96@gmail.com>
2023-08-06 23:43:57 -0400

    not found page fix

--- frontend/src/components/NotFoundPage/index.jsx
+++ frontend/src/components/NotFoundPage/index.jsx
@@ -26,64 +26,66 @@ function NotFoundPage() {
 
   return (
     <>
-      <div className="nav-bar-container">
-        <div onClick={sendToLanding} className="nav-bar-logo">
-          <i className="fa-brands fa-discord fa-xl"></i>
-          <p
-            style={{
-              marginBottom: "0px",
-              paddingLeft: ".5rem",
-              fontWeight: "800",
-              fontSize: "16px",
-            }}
-          >
-            Slacord
+      <div className="nav-bar-scroll-container">
+        <div className="nav-bar-container">
+          <div onClick={sendToLanding} className="nav-bar-logo">
+            <i className="fa-brands fa-discord fa-xl"></i>
+            <p
+              style={{
+                marginBottom: "0px",
+                paddingLeft: ".5rem",
+                fontWeight: "800",
+                fontSize: "16px",
+              }}
+            >
+              Slacord
+            </p>
+          </div>
+          <div className="nav-bar-links">
+            <p onClick={sendToApp}>Download</p>
+            <p onClick={sendToApp}>Nitro</p>
+            <p onClick={sendToApp}>Discover</p>
+            <p onClick={sendToApp}>Safety</p>
+            <p onClick={sendToApp}>Support</p>
+            <p onClick={sendToApp}>Blog</p>
+            <p onClick={sendToApp}>Careers</p>
+          </div>
+          <div className="nav-bar-login">
+            {sessionUser ? (
+              <button className="nav-bar-main-button" onClick={sendToMain}>
+                Open Discord
+              </button>
+            ) : (
+              <button className="nav-bar-login-button" onClick={sendToLogin}>
+                Login
+              </button>
+            )}
+            {
+              <button
+                onClick={() =>
+                  alert(
+                    "Mobile application coming soon! Please view the site on an iPad or computer for now."
+                  )
+                }
+                className="nav-bar-mobile-view"
+              >
+                Open Discord
+              </button>
+            }
+          </div>
+        </div>
+        <div className="not-found-body-container">
+          <img src="/images/not_found_page.gif"></img>
+          <h1>WRONG TURN?</h1>
+          <p style={{ color: "#000000" }}>
+            You look lost, stranger. You know what helps when you’re lost? A
+            piping hot bowl of noodles. Take a seat, we’re frantically at work
+            here cooking up something good. Oh, you need something to read?
+            These might help you:
           </p>
         </div>
-        <div className="nav-bar-links">
-        <p onClick={sendToApp}>Download</p>
-        <p onClick={sendToApp}>Nitro</p>
-        <p onClick={sendToApp}>Discover</p>
-        <p onClick={sendToApp}>Safety</p>
-        <p onClick={sendToApp}>Support</p>
-        <p onClick={sendToApp}>Blog</p>
-        <p onClick={sendToApp}>Careers</p>
-        </div>
-        <div className="nav-bar-login">
-          {sessionUser ? (
-            <button className="nav-bar-main-button" onClick={sendToMain}>
-              Open Discord
-            </button>
-          ) : (
-            <button className="nav-bar-login-button" onClick={sendToLogin}>
-              Login
-            </button>
-          )}
-          {
-            <button
-              onClick={() =>
-                alert(
-                  "Mobile application coming soon! Please view the site on an iPad or computer for now."
-                )
-              }
-              className="nav-bar-mobile-view"
-            >
-              Open Discord
-            </button>
-          }
-        </div>
+        <Footer />
       </div>
-      <div className="not-found-body-container">
-        <img src="/images/not_found_page.gif"></img>
-        <h1>WRONG TURN?</h1>
-        <p style={{ color: "#000000" }}>
-          You look lost, stranger. You know what helps when you’re lost? A
-          piping hot bowl of noodles. Take a seat, we’re frantically at work
-          here cooking up something good. Oh, you need something to read? These
-          might help you:
-        </p>
-      </div>
-      <Footer />
     </>
   );
 }
